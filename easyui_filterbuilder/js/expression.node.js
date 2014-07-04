0e2ed0805a43b28e4a20821e5353b57ec1c9f330
Kain <ghostsea@126.com>
2014-07-04 12:39:11 +0800

    初始化项目 Signed-off-by: Kain <ghostsea@126.com>

---
+++ easyui_filterbuilder/js/expression.node.js
@@ -0,0 +1,123 @@
+﻿$(function () {
+    var index = 0;
+    var nodeOptions = [{text:'等于',name:'equals'},
+                                {text:'不等',name:'notequals'},
+                                {text:'大于',name:'greatthan'},
+                                { text: '大于等于', name: 'greatthanequals' },
+                                {text:'小于',name:'lessthan'},
+                                {text:'小于等于',name:'lessthanequals'},
+                                {text:'在...之间',name:'between'},
+                                { text: '不在...之间', name: 'notbetween' },
+                                {text:'包含',name:'contains'},
+                                {text:'不包含',name:'notcontains'},
+                                {text:'以...开始',name:'startWith'},
+                                {text:'以...结尾',name:'endWith'},
+                                {text:'相似',name:'like'},
+                                {text:'不相似',name:'notlike'},
+                                {text:'为空',name:'empty'},
+                                {text:'不为空',name:'notempty'}];
+
+    function findField(fields,name)
+    {
+        var result = null;
+        $.each(fields, function (i, row) {
+            if (row.name == name)
+                result = row;
+        });
+        return result;
+    }
+
+    function node(target) {
+        options = $.data(target, 'expressionnode').options;
+        target = $(target);
+        target.addClass('expression-node');
+        var fieldMenuButton = $("<a></a>").appendTo(target);
+        var optionMenuButton = $("<a></a>").appendTo(target);
+        var editorDiv = $("<span></span>").appendTo(target);
+        var btn = $("<a></a>")
+                .appendTo(target)
+                .linkbutton({
+                    plain: true,
+                    text: '',
+                    iconCls: 'icon-remove',
+                    onClick: function () { target.remove(); }
+                });
+
+        var fields = options.fields;
+        var fieldMenu = $("<div></div>").attr('id', "field_menu_" + index).appendTo(target);
+        var opMenu = $("<div></div>").attr('id', "field_menu2_" + index).appendTo(target);
+
+        if( options.node == null)
+            options.node = { field: fields[0] };
+        options.node.op = nodeOptions[0];
+        createEditor(options.node, editorDiv);
+
+        fieldMenu.buildmenu({
+            data: fields,
+            onClick: function (item) {
+                options.node = { field: findField(options.fields, item.id) };
+                options.node.op = nodeOptions[0];
+                fieldMenuButton.menubutton({
+                    text: options.node.field.text,
+                });
+            }
+        });
+        opMenu.buildmenu({
+            data: nodeOptions,
+            onClick: function (item) {
+                options.node.op = { text: item.text, name: item.name };
+                optionMenuButton.menubutton({
+                    text: options.node.op.text,
+                });
+            }
+        });
+
+        optionMenuButton.menubutton({
+            text: options.node.op.text,
+            menu: "#field_menu2_"  + index
+        });
+
+        fieldMenuButton.menubutton({
+            text: options.node.field.text,
+            menu: '#field_menu_' + index
+        });
+        index = index + 1;
+        return target;
+    };
+
+    function createEditor(node, target) {
+        var editor = getEditor();
+        editor.init(target, null);
+    };
+
+    function getEditor(dataType) {
+        var editors = $.fn.datagrid.defaults.editors;
+        return editors.text;
+    };
+
+    $.fn.expressionnode = function (options, params) {
+        if (typeof options == 'string') {
+            return $.fn.linkbutton.methods[options](this, param);
+        };
+        options = options || {};
+        return this.each(function () {
+            if (!$.data(this, 'expressionnode')) {
+                $.data(this, 'expressionnode', {
+                    options: $.extend({}, $.fn.expressionnode.defaults, options)
+                });
+            }
+            node(this, options);
+        });
+    };
+
+    $.fn.expressionnode.defaults = {
+        data: []
+    };
+
+    $.fn.expressionnode.methods = {
+        options: function (jq) {
+            return $.data(jq[0], 'expressionnode').options;
+        }
+    };
+
+})
---
+++ easyui_filterbuilder/js/filterBuilder.js
@@ -0,0 +1,122 @@
+﻿$(function ($) {
+    var menuContent = "<div data-options=\"name:'and'\">And</div>" +
+                "<div data-options=\"name:'or'\">Or</div>" +
+                "<div data-options=\"name:'notand'\">Not And</div>" +
+                "<div data-options=\"name:'notor'\">Not Or</div>" +
+                "<div class=\"menu-sep\"></div>" +
+                "<div data-options=\"name:'group'\">Add Group</div>" +
+                "<div data-options=\"name:'condition'\">Add Condition</div>" +
+                "<div class=\"menu-sep\"></div>" +
+                "<div data-options=\"name:'remove'\">Remove</div>";
+
+    function createGroup(target) {
+        var options = $.data(target, 'expressiongroup').options;
+        panel = $(target);
+        var index = panel.parent().children('.expression-group').length + 1;
+        panel.addClass("expression-group");
+        var menubutton = $("<a></a>").text(options.type)
+                                                            .appendTo(panel);
+
+        var addButton = $("<a></a>").appendTo(panel)
+                                                       .linkbutton({
+                                                           plain: true,
+                                                           text: '',
+                                                           iconCls: 'icon-add',
+                                                           onClick: function () { addNode();}
+                                                       });
+
+        var menu = $('<div></div>')
+                            .attr('id', 'menu_' + index)
+                            .html(menuContent).appendTo(panel);
+
+        var nodesPanel = $('<div></div>').appendTo(panel);
+
+        menubutton.menubutton({ menu: '#menu_' + index });
+        menu.menu({
+            onClick: menuClick
+        });
+
+        var menuClick = function (item) {
+            var name = item.name;
+            if (name == "remove") {
+                var groups = panel.parent().children('.expression-group');
+                if (groups.length == 1) {
+                    nodesPanel.empty();
+                }
+                else {
+                    panel.remove();
+                }
+            }
+            else if (name == "group") {
+                var div = $("<div></div>").appendTo(panel.parent());
+                div.expressionGroup({ fields: options.fields });
+            }
+            else {
+                self.menubutton.menubutton({ 'text': item.text });
+                this.type = item.text;
+            }
+        };
+
+        var addNode = function (node) {
+            var div = $("<div></div>").appendTo(nodesPanel);
+            div.expressionnode({ fields: options.fields, node: node });
+        };
+
+        $.each(options.nodes, function (i, node) {
+            addNode(node);
+        });
+    };     
+
+    $.fn.expressionGroup = function (options, param) {
+        options = options || {};
+        return this.each(function () {
+            if (!$.data(this, 'expressiongroup')) {
+                $.data(this, 'expressiongroup', {
+                    options: $.extend({}, $.fn.expression.defaults, options)
+                });
+            }
+            createGroup(this);
+        });
+    };
+
+    $.fn.expressionGroup.methods = {
+        options: function (jq) {
+            return $.data(jq[0], 'expressionGroup').options;
+        }
+    };
+
+    $.fn.expressionGroup.defaults = {};
+});
+
+$(function ($) {
+    
+    function addGroup(target) {
+        var options = $.data(target, 'expression').options;
+        var groups = options.groups;
+        var fields = options.fields;
+        $.each(groups, function (i, group) {
+            var op = $.extend({ fields: fields }, group);
+            $("<div></div>").appendTo($(target)).expressionGroup(op);
+        });
+    };
+
+    $.fn.expression = function (options, param) {
+        options = options || {};
+        return this.each(function () {
+            if (!$.data(this, 'expression')) {
+                $.data(this, 'expression', {
+                    options: $.extend({}, $.fn.expression.defaults, options)
+                });
+            }
+            addGroup(this);
+        });
+    };
+
+    $.fn.expression.methods = {
+        options: function (jq) {
+            return $.data(jq[0], 'expression').options;
+        }
+    };
+
+    $.fn.expression.defaults = {};
+})
